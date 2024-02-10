e62dd534371739f7b722be2ad29d395eb0c67fb5
Logan <logan.moon.hansen@gmail.com>
2024-02-10 16:30:08 -0700

    colored trainer card badges

--- engine/gfx/cgb_layouts.asm
+++ engine/gfx/cgb_layouts.asm
@@ -641,9 +641,11 @@ _CGB_TrainerCard:
 	ld a, PRYCE
 	call GetTrainerPalettePointer
 	call LoadPalette_White_Col1_Col2_Black
-	ld a, PREDEFPAL_CGB_BADGE
-	call GetPredefPal
-	call LoadHLPaletteIntoDE
+	ld h1, .BadgePalettes
+	ld bc, 8 palettes
+	ld a, BANK(wOBPals1)
+	call FarCopyWRAM
+
 
 	; fill screen with opposite-gender palette for the card border
 	hlcoord 0, 0, wAttrmap
@@ -719,6 +721,9 @@ _CGB_TrainerCard:
 	ld a, TRUE
 	ldh [hCGBPalUpdate], a
 	ret
+
+.BadgePalettes:
+INCLUDE "gfx/trainer_card/badges.pal"
 
 _CGB_MoveList:
 	ld de, wBGPals1

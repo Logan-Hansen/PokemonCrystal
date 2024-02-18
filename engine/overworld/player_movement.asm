2f9899ffb533ef134cd721355966cacb1b15931e
Logan <logan.moon.hansen@gmail.com>
2024-02-17 22:48:43 -0700

    fixed bump noise being missing in some situations

--- engine/overworld/player_movement.asm
+++ engine/overworld/player_movement.asm
@@ -411,9 +411,10 @@ DoPlayerMovement::
 	db FACE_UP | FACE_LEFT    ; COLL_HOP_UP_LEFT
 
 .CheckWarp:
-; BUG: No bump noise if standing on tile $3E (see docs/bugs_and_glitches.md)
 
 	ld a, [wWalkingDirection]
+	cp STANDING
+	jr z, .not_warp
 	ld e, a
 	ld d, 0
 	ld hl, .EdgeWarps
@@ -425,8 +426,6 @@ DoPlayerMovement::
 	ld a, TRUE
 	ld [wWalkingIntoEdgeWarp], a
 	ld a, [wWalkingDirection]
-	cp STANDING
-	jr z, .not_warp
 
 	ld e, a
 	ld a, [wPlayerDirection]

b24f08482ffe9ea33e1b923e7eee7702e76df128
Logan <logan.moon.hansen@gmail.com>
2024-02-24 23:15:05 -0700

    fixed error

--- data/moves/moves.asm
+++ data/moves/moves.asm
@@ -28,7 +28,7 @@ Moves:
 	move GUILLOTINE,   EFFECT_OHKO,                0, NORMAL,        30,  5,   0
 	move RAZOR_WIND,   EFFECT_RAZOR_WIND,         80, NORMAL,        75, 10,   0
 	move SWORDS_DANCE, EFFECT_ATTACK_UP_2,         0, NORMAL,       100, 30,   0
-	move CUT,          EFFECT_NORMAL_HIT,         50, BUG,          100, 50,   0
+	move CUT,          EFFECT_NORMAL_HIT,         50, BUG,          100, 40,   0
 	move GUST,         EFFECT_GUST,               40, FLYING,       100, 35,   0
 	move WING_ATTACK,  EFFECT_NORMAL_HIT,         60, FLYING,       100, 35,   0
 	move WHIRLWIND,    EFFECT_FORCE_SWITCH,        0, NORMAL,       100, 20,   0

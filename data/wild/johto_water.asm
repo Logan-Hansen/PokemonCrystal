69d434955ab28e6914a792888226454383a87a0e
Logan <logan.moon.hansen@gmail.com>
2024-02-26 10:50:28 -0700

    updated johto water to be game accurate

--- data/wild/johto_water.asm
+++ data/wild/johto_water.asm
@@ -5,8 +5,8 @@ JohtoWaterWildMons:
 	def_water_wildmons RUINS_OF_ALPH_OUTSIDE
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, WOOPER,  	 14,  17
+	db 10, QUAGSIRE, 	 18,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -14,8 +14,8 @@ JohtoWaterWildMons:
 	def_water_wildmons UNION_CAVE_1F
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, WOOPER,  	 14,  17
+	db 10, QUAGSIRE, 	 18,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -23,8 +23,8 @@ JohtoWaterWildMons:
 	def_water_wildmons UNION_CAVE_B1F
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, WOOPER,  	 14,  17
+	db 10, QUAGSIRE, 	 18,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -32,17 +32,17 @@ JohtoWaterWildMons:
 	def_water_wildmons UNION_CAVE_B2F
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 80, TENTACOOL,  	 14,  17
+	db 10, QUAGSIRE, 	 18,  21
+	db 10, TENTACRUEL, 	 18,  21
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons SLOWPOKE_WELL_B1F
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, SLOWPOKE,  	 10,  15
+	db 10, SLOWPOKE, 	 15,  20
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -50,8 +50,8 @@ JohtoWaterWildMons:
 	def_water_wildmons SLOWPOKE_WELL_B2F
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, SLOWPOKE,  	 15,  20
+	db 10, SLOWBRO, 	 20,  22
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -59,8 +59,8 @@ JohtoWaterWildMons:
 	def_water_wildmons ILEX_FOREST
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 80, PSYDUCK,  	 10,  15
+	db 20, GOLDUCK,	 	 15,  16
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -68,71 +68,71 @@ JohtoWaterWildMons:
 	def_water_wildmons MOUNT_MORTAR_1F_OUTSIDE
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 40, GOLDEEN,  	 15,  19
+	db 40, MARILL,	 	 17,  21
+	db 20, SEAKING,	 	 20,  21
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons MOUNT_MORTAR_2F_INSIDE
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 40, GOLDEEN,  	 19,  22
+	db 40, MARILL,	 	 24,  26
+	db 20, SEAKING,	 	 24,  26
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons MOUNT_MORTAR_B1F
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 40, GOLDEEN,  	 19,  22
+	db 40, MARILL,	 	 24,  26
+	db 20, SEAKING,	 	 24,  26
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons WHIRL_ISLAND_SW
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 40, TENTACOOL,  	  18, 19
+	db 10, TENTACRUEL, 	  20, 21
+	db 50, HORSEA,	 	  15, 18
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons WHIRL_ISLAND_B2F
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db  0, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db 80, HORSEA,	 	  20, 22
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons WHIRL_ISLAND_LUGIA_CHAMBER
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db  0, TENTACOOL,  	  18, 19
+	db 15, TENTACRUEL, 	  21, 22
+	db 70, HORSEA,	 	  20, 22
+	db 15, SEADRA, 		  20, 22  
 	end_water_wildmons
 
 	def_water_wildmons SILVER_CAVE_ROOM_2
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 20, GOLDEEN,  	 35,  36
+	db 40, SEAKING,	 	 35,  36
+	db 40, GOLDUCK,	 	 35,  35
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons DARK_CAVE_VIOLET_ENTRANCE
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, MAGIKARP,  	 10,  15
+	db 10, MAGIKARP,	  5,  10
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -140,8 +140,8 @@ JohtoWaterWildMons:
 	def_water_wildmons DARK_CAVE_BLACKTHORN_ENTRANCE
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, MAGIKARP,  	 10,  15
+	db 10, MAGIKARP,	  5,  10
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -149,26 +149,26 @@ JohtoWaterWildMons:
 	def_water_wildmons DRAGONS_DEN_B1F
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 80, MAGIKARP,  	 10,  15
+	db 10, MAGIKARP,	 15,  15
+	db 10, DRATINI,	 	 10,  15
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons OLIVINE_PORT
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_30
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -176,8 +176,8 @@ JohtoWaterWildMons:
 	def_water_wildmons ROUTE_31
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -185,26 +185,26 @@ JohtoWaterWildMons:
 	def_water_wildmons ROUTE_32
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 60, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db 20, QUAGSIRE, 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_34
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_35
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 80, PSYDUCK,  	 15,  20
+	db 20, GOLDUCK,	 	 20,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -212,35 +212,35 @@ JohtoWaterWildMons:
 	def_water_wildmons ROUTE_40
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  15, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_41
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  18, 20
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, MANTINE,	 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_42
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
+	db 60, GOLDEEN,  	 15,  20
+	db  0, MARILL,	 	 17,  21
+	db 40, SEAKING,	 	 20,  21
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_43
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, MAGIKARP,  	 10,  15
+	db 10, MAGIKARP,	  5,  10
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -248,8 +248,8 @@ JohtoWaterWildMons:
 	def_water_wildmons ROUTE_44
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 60, POLIWAG,  	 20,  25
+	db 40, POLIWHIRL, 	 25,  26
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -257,8 +257,8 @@ JohtoWaterWildMons:
 	def_water_wildmons ROUTE_45
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, MAGIKARP,  	 10,  15
+	db 10, MAGIKARP,	  5,  10
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -266,26 +266,26 @@ JohtoWaterWildMons:
 	def_water_wildmons NEW_BARK_TOWN
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  15, 20
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, MANTINE,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons CHERRYGROVE_CITY
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  15, 20
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, MANTINE,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons VIOLET_CITY
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -293,26 +293,26 @@ JohtoWaterWildMons:
 	def_water_wildmons CIANWOOD_CITY
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  15, 20
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, MANTINE,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons OLIVINE_CITY
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
-	db  0, WOOPER,	 	  5,   5
-	db  0, QUAGSIRE, 	  5,   5
+	db 80, TENTACOOL,  	  15, 20
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, MANTINE,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ECRUTEAK_CITY
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -320,8 +320,8 @@ JohtoWaterWildMons:
 	def_water_wildmons LAKE_OF_RAGE
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, MAGIKARP,  	 15,  19
+	db 10, GYARADOS,	 20,  20
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -329,8 +329,8 @@ JohtoWaterWildMons:
 	def_water_wildmons BLACKTHORN_CITY
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 90, MAGIKARP,  	 10,  15
+	db 10, MAGIKARP,	  5,  10
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
@@ -338,8 +338,8 @@ JohtoWaterWildMons:
 	def_water_wildmons SILVER_CAVE_OUTSIDE
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, WOOPER,  	  3,   6
-	db 10, WOOPER,	 	  3,   6
+	db 20, POLIWAG,  	 35,  40
+	db 80, POLIWHIRL, 	 35,  40
 	db  0, WOOPER,	 	  5,   5
 	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons

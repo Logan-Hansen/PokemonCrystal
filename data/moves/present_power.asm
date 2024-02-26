fad6e24456d5133050f754c0044e2883266ee594
Logan <logan.moon.hansen@gmail.com>
2024-02-25 17:47:28 -0700

    major buffs and changes to moves, largely following Pokemon Crystal Ultimate changes

--- data/moves/present_power.asm
+++ data/moves/present_power.asm
@@ -1,6 +1,6 @@
 PresentPower:
 	; chance, power
-	db 40 percent,     40 ; 40% chance
-	db 70 percent + 1, 80 ; 30% chance
-	db 80 percent,    120 ; 10% chance
-	db -1                 ; 20% chance to heal instead
+	db 50 percent,     40 ; 50% chance
+	db 80 percent + 1, 80 ; 30% chance
+	db 100 percent,    120 ; 20% chance
+	db -1                 ; 0% chance to heal instead

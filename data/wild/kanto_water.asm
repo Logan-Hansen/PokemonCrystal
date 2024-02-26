19260c21040808492ff2d87f43ae976e62110085
Logan <logan.moon.hansen@gmail.com>
2024-02-26 13:30:10 -0700

    added basic kanto water avaliability

--- data/wild/kanto_water.asm
+++ data/wild/kanto_water.asm
@@ -5,26 +5,26 @@ KantoWaterWildMons:
 	def_water_wildmons TOHJO_FALLS
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 40, GOLDEEN,  	 19,  22
+	db 40, SLOWPOKE,	 24,  26
+	db 20, SEAKING,	 	 24,  26
+	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons VERMILION_PORT
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_4
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 80, GOLDEEN,  	 10,  22
+	db 20, SEAKING,	 	 24,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -32,17 +32,17 @@ KantoWaterWildMons:
 	def_water_wildmons ROUTE_6
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, PSYDUCK,  	 15,  20
+	db 20, GOLDUCK,	 	 20,  21
+	db  0, WOOPER,	 	  5,   5
+	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_9
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 80, GOLDEEN,  	 10,  22
+	db 20, SEAKING,	 	 24,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -50,8 +50,8 @@ KantoWaterWildMons:
 	def_water_wildmons ROUTE_10_NORTH
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 80, GOLDEEN,  	 10,  22
+	db 20, SEAKING,	 	 24,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -59,62 +59,62 @@ KantoWaterWildMons:
 	def_water_wildmons ROUTE_12
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 60, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db 20, QUAGSIRE, 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_13
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 60, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db 20, QUAGSIRE, 	  20, 22
+	db  0, SEADRA, 		  20, 22 
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_19
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_20
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_21
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_22
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
+	db  0, WOOPER,	 	  5,   5
+	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_24
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 80, GOLDEEN,  	 10,  22
+	db 20, SEAKING,	 	 24,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -122,8 +122,8 @@ KantoWaterWildMons:
 	def_water_wildmons ROUTE_25
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 80, GOLDEEN,  	 10,  22
+	db 20, SEAKING,	 	 24,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -131,53 +131,53 @@ KantoWaterWildMons:
 	def_water_wildmons ROUTE_26
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_27
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons ROUTE_28
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
+	db  0, WOOPER,	 	  5,   5
+	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons PALLET_TOWN
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons VIRIDIAN_CITY
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 70, POLIWAG,  	 15,  20
+	db 30, POLIWHIRL, 	 20,  21
+	db  0, WOOPER,	 	  5,   5
+	db  0, QUAGSIRE, 	  5,   5
 	end_water_wildmons
 
 	def_water_wildmons CERULEAN_CITY
 	db 4 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 80, GOLDEEN,  	 10,  22
+	db 20, SEAKING,	 	 24,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -185,17 +185,17 @@ KantoWaterWildMons:
 	def_water_wildmons VERMILION_CITY
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	def_water_wildmons CELADON_CITY
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 90, GRIMER,  	 19,  22
+	db 10, MUK,	  		 23,  26
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -203,8 +203,8 @@ KantoWaterWildMons:
 	def_water_wildmons FUCHSIA_CITY
 	db 2 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
+	db 90, MAGIKARP,  	 15,  20
+	db 10, MAGIKARP,	 10,  15
 	db  0, MAGIKARP, 	  5,   5
 	db  0, MAGIKARP, 	  5,   5
 	end_water_wildmons
@@ -212,10 +212,10 @@ KantoWaterWildMons:
 	def_water_wildmons CINNABAR_ISLAND
 	db 6 percent ; encounter rate
 	;  %, species,		min, max
-	db 90, MAGIKARP,  	  3,   6
-	db 10, MAGIKARP,	  3,   6
-	db  0, MAGIKARP, 	  5,   5
-	db  0, MAGIKARP, 	  5,   5
+	db 80, TENTACOOL,  	  18, 19
+	db 20, TENTACRUEL, 	  21, 22
+	db  0, HORSEA,	 	  20, 22
+	db  0, SEADRA, 		  20, 22
 	end_water_wildmons
 
 	db -1 ; end

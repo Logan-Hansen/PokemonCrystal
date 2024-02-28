0aad409d545a0dd294fd5a10a29fabd02bb81c16
Logan <logan.moon.hansen@gmail.com>
2024-02-27 17:58:09 -0700

    fixed warp tile, was going out wrong side

--- maps/Route2.asm
+++ maps/Route2.asm
@@ -154,8 +154,8 @@ Route2_MapEvents:
 	warp_event 17, 27, ROUTE_2_GATE, 2
 	warp_event 12,  7, DIGLETTS_CAVE, 3
 	warp_event  2,  9, VIRIDIAN_FOREST, 1
-	warp_event  3,  9, VIRIDIAN_FOREST, 1
-	warp_event  3, 33, VIRIDIAN_FOREST, 2
+	warp_event  3,  9, VIRIDIAN_FOREST, 2
+	warp_event  3, 33, VIRIDIAN_FOREST, 3
 
 	def_coord_events
 

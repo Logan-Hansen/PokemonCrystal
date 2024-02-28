978bdd5c9f5fe017b82416e6c6335cdf9705cbf6
Logan <logan.moon.hansen@gmail.com>
2024-02-27 18:22:36 -0700

    put elms house back instead of testing warp point

--- maps/NewBarkTown.asm
+++ maps/NewBarkTown.asm
@@ -286,8 +286,7 @@ NewBarkTown_MapEvents:
 	warp_event  6,  3, ELMS_LAB, 1
 	warp_event 13,  5, PLAYERS_HOUSE_1F, 1
 	warp_event  3, 11, PLAYERS_NEIGHBORS_HOUSE, 1
-	warp_event 10, 12, ELMS_HOUSE, 1
-	warp_event 11, 13, VIRIDIAN_FOREST, 1
+	warp_event 11, 13, ELMS_HOUSE, 1
 
 	def_coord_events
 	coord_event  1,  8, SCENE_NEWBARKTOWN_TEACHER_STOPS_YOU, NewBarkTown_TeacherStopsYouScene1

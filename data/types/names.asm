7ef6d97e5a6525cf208b8589e27b8c155b63f5d7
Logan <logan.moon.hansen@gmail.com>
2024-03-03 16:41:01 -0700

    forgot to move ghost type too

--- data/types/names.asm
+++ data/types/names.asm
@@ -10,7 +10,6 @@ TypeNames:
 	dw Rock
 	dw Bird
 	dw Bug
-	dw Ghost
 	dw Steel
 	dw Dark
 	assert_table_length UNUSED_TYPES
@@ -28,6 +27,7 @@ endr
 	dw Psychic
 	dw Ice
 	dw Dragon
+	dw Ghost
 	assert_table_length TYPES_END
 
 Normal:    db "NORMAL@"

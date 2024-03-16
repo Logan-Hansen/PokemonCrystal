cf172b5ade9032de9e684369a19790e74b6a3f17
Logan <logan.moon.hansen@gmail.com>
2024-03-16 12:10:48 -0600

    updated the rest of Kanto to have headbuttable trees

--- data/wild/treemon_maps.asm
+++ data/wild/treemon_maps.asm
@@ -2,7 +2,8 @@ MACRO treemon_map
 	map_id \1
 	db \2 ; treemon set
 ENDM
-
+; can add new sets in constants/pokemon_data_constants.asm and
+; in data/wild/treemons.asm
 TreeMonMaps:
 	treemon_map ROUTE_26,                  TREEMON_SET_KANTO
 	treemon_map ROUTE_27,                  TREEMON_SET_KANTO
@@ -38,7 +39,42 @@ TreeMonMaps:
 	treemon_map BLACKTHORN_CITY,           TREEMON_SET_CITY
 	treemon_map SILVER_CAVE_OUTSIDE,       TREEMON_SET_CITY
 	treemon_map ILEX_FOREST,               TREEMON_SET_FOREST
-	treemon_map CERULEAN_CITY,			   TREEMON_SET_KANTO
+	treemon_map PALLET_TOWN,			   TREEMON_SET_CITY
+	treemon_map VIRIDIAN_CITY,             TREEMON_SET_CITY
+	treemon_map PEWTER_CITY,			   TREEMON_SET_CITY
+	treemon_map CERULEAN_CITY,			   TREEMON_SET_CITY
+	treemon_map VERMILION_CITY,			   TREEMON_SET_CITY
+	treemon_map LAVENDER_TOWN,			   TREEMON_SET_CITY
+	treemon_map CELADON_CITY,			   TREEMON_SET_CITY
+	treemon_map FUCHSIA_CITY,			   TREEMON_SET_CITY
+	treemon_map SAFFRON_CITY,			   TREEMON_SET_CITY
+	treemon_map CINNABAR_ISLAND,		   TREEMON_SET_CITY
+	treemon_map VIRIDIAN_FOREST,		   TREEMON_SET_KANTO
+	treemon_map ROUTE_1,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_2,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_3,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_4,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_5,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_6,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_7,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_8,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_9,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_10,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_11,			       TREEMON_SET_KANTO
+	treemon_map ROUTE_12,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_13,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_14,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_15,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_16,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_17,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_18,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_19,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_20,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_21,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_22,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_23,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_24,				   TREEMON_SET_KANTO
+	treemon_map ROUTE_25,				   TREEMON_SET_KANTO
 	db -1
 
 RockMonMaps:

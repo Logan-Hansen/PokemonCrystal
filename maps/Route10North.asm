27bf8e4875f3ffe3cf6c58a2ca5bc71d7b22c9dc
logan <logan.moon.hansen@gmail.com>
2024-03-16 23:02:19 -0600

    added in Zapdos

--- maps/Route10North.asm
+++ maps/Route10North.asm
@@ -1,7 +1,44 @@
+	object_const_def
+	const POWER_PLANT_ZAPDOS
+
 Route10North_MapScripts:
 	def_scene_scripts
 
 	def_callbacks
+	callback MAPCALLBACK_OBJECTS, PowerPlantZapdosCallback
+
+PowerPlantZapdosCallback:
+	checkevent EVENT_POWER_PLANT_ZAPDOS
+	iftrue .NoAppear
+	iffalse .Appear
+	sjump .NoAppear
+
+.Appear:
+	appear POWER_PLANT_ZAPDOS
+	endcallback
+
+.NoAppear:
+	disappear POWER_PLANT_ZAPDOS
+	endcallback
+
+Zapdos:
+	faceplayer
+	opentext
+	writetext ZapdosText
+	cry ZAPDOS
+	pause 15
+	closetext
+	setevent EVENT_FOUGHT_ZAPDOS
+	loadvar VAR_BATTLETYPE, BATTLETYPE_FORCEITEM
+	loadwildmon ZAPDOS, 60
+	startbattle
+	disappear POWER_PLANT_ZAPDOS
+	reloadmapafterbattle
+	end
+
+ZapdosText:
+	text "Gyaoo!"
+	done
 
 PowerPlantSign:
 	jumptext PowerPlantSignText
@@ -27,3 +64,5 @@ Route10North_MapEvents:
 	bg_event 12,  1, BGEVENT_READ, Route10PokecenterSign
 
 	def_object_events
+	object_event  7, 10, SPRITE_ZAPDOS, SPRITEMOVEDATA_POKEMON, 0, 0, -1, -1, PAL_NPC_BROWN, OBJECTTYPE_SCRIPT, 0, ZAPDOS, EVENT_POWER_PLANT_ZAPDOS
+

efcad16cb9349035b395f889a288efb9c9828480
Logan Hansen <86215864+Logan-Hansen@users.noreply.github.com>
2024-03-21 15:16:07 -0600

    Update class_names.asm
    reordered how I want them, still need to update parties.asm and trainer_constants.asm to match this ordering

--- data/trainers/class_names.asm
+++ data/trainers/class_names.asm
@@ -2,60 +2,59 @@ TrainerClassNames::
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	list_start TrainerClassNames
 	li "LEADER" ; Falkner
+    li "LEADER" ; Bugsy
 	li "LEADER" ; Whitney
-	li "LEADER" ; Bugsy
 	li "LEADER" ; Morty
+    li "LEADER" ; Chuck
+    li "LEADER" ; Jasmine
 	li "LEADER" ; Pryce
-	li "LEADER" ; Jasmine
-	li "LEADER" ; Chuck
 	li "LEADER" ; Clair
-	li "RIVAL"  ; rival1
-	li "#MON PROF." ; professor
-	li "ELITE FOUR" ; Will
-	li "<PKMN> TRAINER" ; Cal
-	li "ELITE FOUR" ; Bruno
-	li "ELITE FOUR" ; Karen
-	li "ELITE FOUR" ; Koga
-	li "CHAMPION" ; Lance
-	li "LEADER" ; Brock
+	li "RIVAL"  ; Rival1
+    li "RIVAL" ; Rival2
+    li "ELITE FOUR" ; Will E4
+    li "ELITE FOUR" ; Koga
+    li "ELITE FOUR" ; Bruno
+    li "ELITE FOUR" ; Karen
+    li "CHAMPION" ; Lance
+    li "#MON PROF." ; professor
+    li "<PKMN> TRAINER" ; Cal
+    li "LEADER" ; Brock
 	li "LEADER" ; Misty
 	li "LEADER" ; Surge
+    li "LEADER" ; Erika
+    li "LEADER" ; Janine
+	li "LEADER" ; Sabrina
+    li "LEADER" ; Blaine
+    li "LEADER" ; Blue
+	li "<PKMN> TRAINER" ; Red
 	li "SCIENTIST"
-	li "LEADER" ; Erika
 	li "YOUNGSTER"
 	li "SCHOOLBOY"
 	li "BIRD KEEPER"
 	li "LASS"
-	li "LEADER" ; Janine
-	li "COOLTRAINER"
-	li "COOLTRAINER"
+	li "COOLTRAINER" ;male
+	li "COOLTRAINER" ;female
 	li "BEAUTY"
 	li "#MANIAC"
-	li "ROCKET"
 	li "GENTLEMAN"
 	li "SKIER"
 	li "TEACHER"
-	li "LEADER" ; Sabrina
 	li "BUG CATCHER"
 	li "FISHER"
 	li "SWIMMER♂"
 	li "SWIMMER♀"
 	li "SAILOR"
 	li "SUPER NERD"
-	li "RIVAL" ; Rival2
 	li "GUITARIST"
 	li "HIKER"
 	li "BIKER"
-	li "LEADER" ; Blaine
 	li "BURGLAR"
 	li "FIREBREATHER"
 	li "JUGGLER"
 	li "BLACKBELT"
-	li "ROCKET"
 	li "PSYCHIC"
 	li "PICNICKER"
 	li "CAMPER"
-	li "ROCKET"
 	li "SAGE"
 	li "MEDIUM"
 	li "BOARDER"
@@ -63,9 +62,10 @@ TrainerClassNames::
 	li "KIMONO GIRL"
 	li "TWINS"
 	li "#FAN"
-	li "<PKMN> TRAINER" ; Red
-	li "LEADER" ; Blue
 	li "OFFICER"
-	li "ROCKET"
+	li "ROCKET" ;gruntM
+	li "ROCKET" ;executiveM
+    li "ROCKET" ;gruntF
+    li "ROCKET" ;executiveF
 	li "MYSTICALMAN"
 	assert_list_length NUM_TRAINER_CLASSES

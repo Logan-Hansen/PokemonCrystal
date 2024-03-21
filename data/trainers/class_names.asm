d363f23536124ef45ea00bdae89cac6e1af44f9e
Logan <logan.moon.hansen@gmail.com>
2024-03-21 16:15:22 -0600

    putting these back in the original order so I don't have to redo the ordering on all the other files

--- data/trainers/class_names.asm
+++ data/trainers/class_names.asm
@@ -2,59 +2,60 @@ TrainerClassNames::
 ; entries correspond to trainer classes (see constants/trainer_constants.asm)
 	list_start TrainerClassNames
 	li "LEADER" ; Falkner
-	li "LEADER" ; Bugsy
 	li "LEADER" ; Whitney
+	li "LEADER" ; Bugsy
 	li "LEADER" ; Morty
-	li "LEADER" ; Chuck
-	li "LEADER" ; Jasmine
 	li "LEADER" ; Pryce
+	li "LEADER" ; Jasmine
+	li "LEADER" ; Chuck
 	li "LEADER" ; Clair
-	li "RIVAL"  ; Rival1
-	li "RIVAL" ; Rival2
+	li "RIVAL"  ; rival1
+	li "#MON PROF." ; professor
 	li "ELITE FOUR" ; Will
-	li "ELITE FOUR" ; Koga
+	li "<PKMN> TRAINER" ; Cal
 	li "ELITE FOUR" ; Bruno
 	li "ELITE FOUR" ; Karen
+	li "ELITE FOUR" ; Koga
 	li "CHAMPION" ; Lance
-	li "#MON PROF." ; professor
-	li "<PKMN> TRAINER" ; Cal
 	li "LEADER" ; Brock
 	li "LEADER" ; Misty
 	li "LEADER" ; Surge
-	li "LEADER" ; Erika
-	li "LEADER" ; Janine
-	li "LEADER" ; Sabrina
-	li "LEADER" ; Blaine
-	li "LEADER" ; Blue
-	li "<PKMN> TRAINER" ; Red
 	li "SCIENTIST"
+	li "LEADER" ; Erika
 	li "YOUNGSTER"
 	li "SCHOOLBOY"
 	li "BIRD KEEPER"
 	li "LASS"
-	li "COOLTRAINER" ;male
-	li "COOLTRAINER" ;female
+	li "LEADER" ; Janine
+	li "COOLTRAINER"
+	li "COOLTRAINER"
 	li "BEAUTY"
 	li "#MANIAC"
+	li "ROCKET"
 	li "GENTLEMAN"
 	li "SKIER"
 	li "TEACHER"
+	li "LEADER" ; Sabrina
 	li "BUG CATCHER"
 	li "FISHER"
 	li "SWIMMER♂"
 	li "SWIMMER♀"
 	li "SAILOR"
 	li "SUPER NERD"
+	li "RIVAL" ; Rival2
 	li "GUITARIST"
 	li "HIKER"
 	li "BIKER"
+	li "LEADER" ; Blaine
 	li "BURGLAR"
 	li "FIREBREATHER"
 	li "JUGGLER"
 	li "BLACKBELT"
+	li "ROCKET"
 	li "PSYCHIC"
 	li "PICNICKER"
 	li "CAMPER"
+	li "ROCKET"
 	li "SAGE"
 	li "MEDIUM"
 	li "BOARDER"
@@ -62,10 +63,9 @@ TrainerClassNames::
 	li "KIMONO GIRL"
 	li "TWINS"
 	li "#FAN"
+	li "<PKMN> TRAINER" ; Red
+	li "LEADER" ; Blue
 	li "OFFICER"
-	li "ROCKET" ;gruntM
-	li "ROCKET" ;executiveM
-	li "ROCKET" ;gruntF
-	li "ROCKET" ;executiveF
+	li "ROCKET"
 	li "MYSTICALMAN"
 	assert_list_length NUM_TRAINER_CLASSES

5206a11b39b874701f4dcdd4d032429c4b590ffa
Logan <logan.moon.hansen@gmail.com>
2024-03-23 22:52:44 -0600

    dramatically increased how much haircuts increase happiness

--- data/events/happiness_changes.asm
+++ data/events/happiness_changes.asm
@@ -2,23 +2,23 @@ HappinessChanges:
 ; entries correspond to HAPPINESS_* constants
 	table_width 3, HappinessChanges
 	; change if happiness < 100, change if happiness < 200, change otherwise
-	db  +5,  +3,  +2 ; Gained a level
+	db +10, +10,  +5 ; Gained a level
 	db  +5,  +3,  +2 ; Vitamin
 	db  +1,  +1,  +0 ; X Item
-	db  +3,  +2,  +1 ; Battled a Gym Leader
-	db  +1,  +1,  +0 ; Learned a move
+	db +20, +10,  +2 ; Battled a Gym Leader
+	db  +5,  +1,  +0 ; Learned a move
 	db  -1,  -1,  -1 ; Lost to an enemy
 	db  -5,  -5, -10 ; Fainted due to poison
 	db  -5,  -5, -10 ; Lost to a much stronger enemy
-	db  +2,  +2,  +2 ; Haircut (older brother) 1
-	db  +6,  +6,  +2 ; Haircut (older brother) 2
-	db +10, +10,  +4 ; Haircut (older brother) 3
-	db  +2,  +2,  +2 ; Haircut (younger brother) 1
-	db  +6,  +6,  +2 ; Haircut (younger brother) 2
-	db +20, +20,  +8 ; Haircut (younger brother) 3
+	db +20, +10,  +8 ; Haircut (older brother) 1
+	db +20, +20,  +8 ; Haircut (older brother) 2
+	db +30, +30, +16 ; Haircut (older brother) 3
+	db +10, +10,  +8 ; Haircut (younger brother) 1
+	db +20, +20,  +8 ; Haircut (younger brother) 2
+	db +40, +40, +16 ; Haircut (younger brother) 3
 	db  -5,  -5, -10 ; Used Heal Powder or Energypowder (bitter)
 	db -10, -10, -15 ; Used Energy Root (bitter)
 	db -15, -15, -20 ; Used Revival Herb (bitter)
-	db  +6,  +6,  +2 ; Grooming
-	db +10,  +6,  +4 ; Gained a level in the place where it was caught
+	db +20, +20, +20 ; Grooming
+	db +20, +10, +10 ; Gained a level in the place where it was caught
 	assert_table_length NUM_HAPPINESS_CHANGES

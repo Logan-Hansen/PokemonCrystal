309905bfa7a321725b4538df1d3ebc746f37fe83
Logan <logan.moon.hansen@gmail.com>
2024-12-23 12:16:41 -0700

    voltorb trade is now Chinchou gift pokemon

--- maps/OlivineTimsHouse.asm
+++ maps/OlivineTimsHouse.asm
@@ -8,11 +8,85 @@ OlivineTimsHouse_MapScripts:
 
 Tim:
 	faceplayer
+	;opentext
+	;trade NPC_TRADE_TIM
+	;waitbutton
+	;closetext
+	;end
+	checkevent EVENT_GOT_A_CHINCHOU_FROM_TIM
+	iftrue .AskHowPokemon
 	opentext
-	trade NPC_TRADE_TIM
+	writetext TimGivePokemonText
+	yesorno
+	iffalse .RefusedPokemon
+	readvar VAR_PARTYCOUNT
+	ifequal PARTY_LENGTH, .PartyFull
+	givepoke CHINCHOU, 20, METAL_COAT
+	setevent EVENT_GOT_A_CHINCHOU_FROM_TIM
+	writetext VioletKyleGaveOnixText
 	waitbutton
 	closetext
 	end
+
+.AskHowPokemon: ;added
+	opentext
+	writetext TimHowDoingText
+	waitbutton
+	closetext
+	end
+
+.RefusedPokemon: ;added
+	writetext TimRefusedText
+	waitbutton
+	closetext
+	end
+
+.PartyFull: ;added
+	writetext TimFullPartyText
+	waitbutton
+	closetext
+	end
+
+
+TimGivePokemonText: ;added
+	text "I train CHINCHOU,"
+	line "but I don't get"
+	cont "out much anymore."
+	
+	para "If you don't mind,"
+	line "could you raise it"
+	cont "for me?"
+	done
+
+TimHowDoingText: ;added
+	text "How is CHINCHOU?"
+	
+	para "Thanks again for"
+	line "raising it!"
+	done
+
+TimRefusedText: ;added
+	text "Oh..."
+
+	para "Well, let me know"
+	line "if you change"
+	cont "your mind..."
+
+TimFullPartyText: ;added
+	text "Oh, no. You can't"
+	line "carry any more"
+	cont "#MON with you."
+
+	para "I will be here"
+	line "if you ever want"
+	cont "CHINCHOU."
+	done
+
+TimGavePokemonText: ;added
+	para "I am glad you're"
+	line "going to raise"
+	cont "it now. Thanks!"
+	done	
 
 TimsHouseBookshelf:
 	jumpstd MagazineBookshelfScript
